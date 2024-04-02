253f844e4b2b5da3c0843e548e26d3ba3e911233
actions <actions@github.com>
2024-04-02 14:32:01 +0000

    Deploying to main from  @ 8e59ea737dc6dd761dc18c775ba194a47ff52a47 🚀

--- output_cl_function_test_environment_clas_locals_mjs.bundle.js
+++ output_cl_function_test_environment_clas_locals_mjs.bundle.js
@@ -46,9 +46,9 @@ class lcl_input_arguments {
       usesTableLine: false,
       withKeySimple: {"name": name}});
     if (abap.compare.ne(abap.builtin.sy.get().subrc, abap.IntegerFactory.get(0))) {
-      const unique236 = await (new abap.Classes['CX_FTD_PARAMETER_NOT_FOUND']()).constructor_();
-      unique236.EXTRA_CX = {"INTERNAL_FILENAME": "cl_function_test_environment.clas.locals_imp.abap","INTERNAL_LINE": 19};
-      throw unique236;
+      const unique238 = await (new abap.Classes['CX_FTD_PARAMETER_NOT_FOUND']()).constructor_();
+      unique238.EXTRA_CX = {"INTERNAL_FILENAME": "cl_function_test_environment.clas.locals_imp.abap","INTERNAL_LINE": 19};
+      throw unique238;
     }
     result.set(ls_row.get().value);
     return result;
@@ -65,9 +65,9 @@ class lcl_input_arguments {
       usesTableLine: false,
       withKeySimple: {"name": name}});
     if (abap.compare.ne(abap.builtin.sy.get().subrc, abap.IntegerFactory.get(0))) {
-      const unique237 = await (new abap.Classes['CX_FTD_PARAMETER_NOT_FOUND']()).constructor_();
-      unique237.EXTRA_CX = {"INTERNAL_FILENAME": "cl_function_test_environment.clas.locals_imp.abap","INTERNAL_LINE": 28};
-      throw unique237;
+      const unique239 = await (new abap.Classes['CX_FTD_PARAMETER_NOT_FOUND']()).constructor_();
+      unique239.EXTRA_CX = {"INTERNAL_FILENAME": "cl_function_test_environment.clas.locals_imp.abap","INTERNAL_LINE": 28};
+      throw unique239;
     }
     result.set(ls_row.get().value);
     return result;
@@ -170,12 +170,12 @@ class lcl_invoker {
       abap.statements.insertInternal({data: ls_table, table: lo_arguments.get().mt_tables});
     }
     await answer.get().if_ftd_invocation_answer$answer({arguments_: li_arguments, result: li_result});
-    for await (const unique238 of abap.statements.loop(lo_result.get().mt_exporting)) {
-      ls_exporting.set(unique238);
+    for await (const unique240 of abap.statements.loop(lo_result.get().mt_exporting)) {
+      ls_exporting.set(unique240);
       fminput.importing[ls_exporting.get().name.get().toLowerCase().trimEnd()].set(ls_exporting.get().value.dereference());
     }
-    for await (const unique239 of abap.statements.loop(lo_result.get().mt_tables)) {
-      ls_table.set(unique239);
+    for await (const unique241 of abap.statements.loop(lo_result.get().mt_tables)) {
+      ls_table.set(unique241);
       fminput.tables[ls_table.get().name.get().toLowerCase().trimEnd()].set(ls_table.get().value.dereference());
     }
   }
