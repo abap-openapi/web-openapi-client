2c468093689cc1d9c48864e7b5b67fa7c0730730
actions <actions@github.com>
2024-07-19 07:07:58 +0000

    Deploying to main from  @ 72da745132f16e0fabf7221320809ea4723eab1d 🚀

--- output_cl_abap_conv_codepage_clas_locals_mjs.bundle.js
+++ output_cl_abap_conv_codepage_clas_locals_mjs.bundle.js
@@ -19,14 +19,18 @@ class lcl_in {
   static INTERNAL_TYPE = 'CLAS';
   static INTERNAL_NAME = 'CLAS-CL_ABAP_CONV_CODEPAGE-LCL_IN';
   static IMPLEMENTED_INTERFACES = ["IF_ABAP_CONV_IN"];
-  static ATTRIBUTES = {};
-  static METHODS = {};
+  static ATTRIBUTES = {"MV_ENCODING": {"type": () => {return new abap.types.Character(20, {"qualifiedName":"abap_encoding"});}, "visibility": "I", "is_constant": " ", "is_class": " "}};
+  static METHODS = {"CONSTRUCTOR": {"visibility": "U", "parameters": {"CODEPAGE": {"type": () => {return new abap.types.Character(20, {"qualifiedName":"abap_encoding"});}, "is_optional": " "}}}};
   constructor() {
     this.me = new abap.types.ABAPObject();
     this.me.set(this);
+    this.mv_encoding = new abap.types.Character(20, {"qualifiedName":"abap_encoding"});
   }
   async constructor_(INPUT) {
-    if (super.constructor_) { await super.constructor_(INPUT); }
+    let codepage = INPUT?.codepage;
+    if (codepage?.getQualifiedName === undefined || codepage.getQualifiedName() !== "ABAP_ENCODING") { codepage = undefined; }
+    if (codepage === undefined) { codepage = new abap.types.Character(20, {"qualifiedName":"abap_encoding"}).set(INPUT.codepage); }
+    this.mv_encoding.set(codepage);
     return this;
   }
   async if_abap_conv_in$convert(INPUT) {
@@ -35,7 +39,7 @@ class lcl_in {
     if (source?.getQualifiedName === undefined || source.getQualifiedName() !== "XSTRING") { source = undefined; }
     if (source === undefined) { source = new abap.types.XString({qualifiedName: "XSTRING"}).set(INPUT.source); }
     let conv = new abap.types.ABAPObject({qualifiedName: "CL_ABAP_CONV_IN_CE", RTTIName: "\\CLASS=CL_ABAP_CONV_IN_CE"});
-    conv.set((await abap.Classes['CL_ABAP_CONV_IN_CE'].create({encoding: new abap.types.Character(5).set('UTF-8')})));
+    conv.set((await abap.Classes['CL_ABAP_CONV_IN_CE'].create({encoding: this.mv_encoding})));
     await conv.get().convert({input: source, data: result});
     return result;
   }
@@ -45,14 +49,18 @@ class lcl_out {
   static INTERNAL_TYPE = 'CLAS';
   static INTERNAL_NAME = 'CLAS-CL_ABAP_CONV_CODEPAGE-LCL_OUT';
   static IMPLEMENTED_INTERFACES = ["IF_ABAP_CONV_OUT"];
-  static ATTRIBUTES = {};
-  static METHODS = {};
+  static ATTRIBUTES = {"MV_ENCODING": {"type": () => {return new abap.types.Character(20, {"qualifiedName":"abap_encoding"});}, "visibility": "I", "is_constant": " ", "is_class": " "}};
+  static METHODS = {"CONSTRUCTOR": {"visibility": "U", "parameters": {"CODEPAGE": {"type": () => {return new abap.types.Character(20, {"qualifiedName":"abap_encoding"});}, "is_optional": " "}}}};
   constructor() {
     this.me = new abap.types.ABAPObject();
     this.me.set(this);
+    this.mv_encoding = new abap.types.Character(20, {"qualifiedName":"abap_encoding"});
   }
   async constructor_(INPUT) {
-    if (super.constructor_) { await super.constructor_(INPUT); }
+    let codepage = INPUT?.codepage;
+    if (codepage?.getQualifiedName === undefined || codepage.getQualifiedName() !== "ABAP_ENCODING") { codepage = undefined; }
+    if (codepage === undefined) { codepage = new abap.types.Character(20, {"qualifiedName":"abap_encoding"}).set(INPUT.codepage); }
+    this.mv_encoding.set(codepage);
     return this;
   }
   async if_abap_conv_out$convert(INPUT) {
@@ -61,7 +69,7 @@ class lcl_out {
     if (source?.getQualifiedName === undefined || source.getQualifiedName() !== "STRING") { source = undefined; }
     if (source === undefined) { source = new abap.types.String({qualifiedName: "STRING"}).set(INPUT.source); }
     let conv = new abap.types.ABAPObject({qualifiedName: "CL_ABAP_CONV_OUT_CE", RTTIName: "\\CLASS=CL_ABAP_CONV_OUT_CE"});
-    conv.set((await abap.Classes['CL_ABAP_CONV_OUT_CE'].create({encoding: new abap.types.Character(5).set('UTF-8')})));
+    conv.set((await abap.Classes['CL_ABAP_CONV_OUT_CE'].create({encoding: this.mv_encoding})));
     await conv.get().convert({data: source, buffer: result});
     return result;
   }
